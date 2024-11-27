ca76c9d3d70f242d812056444965cdb05b7e0f10
Oleksii R <sorrowerkein+bitbucket@gmail.com>
2024-11-27 18:43:16 +0100

    invoice infrastructure - eloquent

---
+++ src/Modules/Invoices/Infrastructure/Models/InvoiceModel.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Modules\Invoices\Infrastructure\Models;
+
+use Eloquent;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Modules\Invoices\Domain\Enums\StatusEnum;
+
+/**
+ * @property string $id
+ * @property string $status
+ * @property string $customer_name
+ * @property string $customer_email
+ * @property Collection<int,InvoiceProductLineModel> $product_lines
+ *
+ * @mixin Eloquent
+ */
+class InvoiceModel extends Model
+{
+    protected $table = 'invoices';
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    protected $fillable = [
+        'status',
+        'customer_name',
+        'customer_email',
+    ];
+
+    protected $casts = [
+        'status' => StatusEnum::class,
+    ];
+
+    public function product_lines() : HasMany
+    {
+        return $this->hasMany(InvoiceProductLineModel::class, 'invoice_id', 'id');
+    }
+}
---
+++ src/Modules/Invoices/Infrastructure/Models/InvoiceProductLineModel.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Modules\Invoices\Infrastructure\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Modules\Invoices\Domain\Entities\Invoice;
+
+/**
+ * @property string $id
+ * @property string $invoice_id
+ * @property string $name
+ * @property int $price
+ * @property int $quantity
+ * @property Invoice $invoice
+ */
+class InvoiceProductLineModel extends Model
+{
+    protected $table = 'invoice_product_lines';
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    protected $fillable = [
+        'invoice_id',
+        'name',
+        'price',
+        'quantity',
+    ];
+
+    protected $casts = [
+        'price'    => "int",
+        'quantity' => "int",
+    ];
+
+    public function invoice() : BelongsTo
+    {
+        return $this->belongsTo(InvoiceModel::class, 'invoice_id', 'id');
+    }
+}
